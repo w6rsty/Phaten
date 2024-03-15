b3736424c17d3d0233295d1fd8aa0cd39e8bb3a5
w6rsty <1486008923@qq.com>
2024-03-15 16:13:31 +0800

    Add more comments to Ptr

--- Phaten/Core/Ptr.hpp
+++ Phaten/Core/Ptr.hpp
@@ -5,6 +5,31 @@
 
 namespace Pt {
 
+/*
+For intrusive reference counting, the object must be based on RefCounted.
+The RefCounted class holds a structure (RefCount) to record the reference 
+count numbers.
+
+Basic rules:
+A SharedPtr will contribute to a strong reference count.
+A WeakPtr will contribute to a weak reference count.
+
+When the STRONG REFERENCE count reduces to zero, a RefCounted-based class
+deconstructs and frees the reference counting structure. For SharedPtr, 
+when it leaves a scope, it will release the strong reference, so no 
+reference exists when the pointed objects exist.
+
+WeakPtr does not contribute to the strong reference, so it does not control
+the  deconstruction of the pointed object. When a RefCounted-based class
+deconstructs, it will check if any WeakPtr still holds a reference to itself
+by checking the weakRefs count. In this case, it will only deconstruct the 
+object if weakRefs != 0, but KEEP the reference counting structure and mark
+its expired flag as true. So a WeakPtr may still exist while its pointed
+object is gone. Therefore, WeakPtr holds a refCount pointer to keep track of
+the object status. When the last WeakPtr is gone, its destructor will be
+responsible for the deallocation of the reference counting structure.
+*/
+
 /// Structure for reference counting
 struct RefCount
 {
@@ -34,6 +59,8 @@ public:
     /// Add a strong reference (Allocate RefCount first time if neccessary).
     void AddRef();
     /// Release a strong reference. Destory the object when last strong reference is gone.
+    /// Only provide function to release strong reference.
+    /// WeakPtr does the weak reference release job 
     virtual void ReleaseRef();
 
     /// Get strong reference count.
@@ -57,6 +84,7 @@ private:
 };
 
 /// Pointer holds a strong reference to a RefCounted subclass.
+/// Destroyed after leave its scope and release the strong reference
 /// Compiled only when T is base of RefCounted.
 template <typename T,
           typename = std::enable_if_t<std::is_base_of_v<RefCounted, T>>>
@@ -156,7 +184,7 @@ template <typename T, typename U>
 SharedPtr<T> StaticCast(const SharedPtr<U>& rhs)
 {
     SharedPtr<T> ret;
-    ret.StaticCast(rhs);
+    ret.StaticCast(rhs); // This pointer should have StaticCast() function.
     return ret;    
 }
 
@@ -165,14 +193,14 @@ template <typename T, typename U>
 SharedPtr<T> DynamicCast(const SharedPtr<U>& rhs)
 {
     SharedPtr<T> ret;
-    ret.DynamicCast(rhs);
+    ret.DynamicCast(rhs); // This pointer should have DynamicCast() function.
     return ret;    
 }
 
 /// Pointer which holds a weak reference to a RefCounted subclass.
 /// Compiled only when T is base of RefCounted.
 template <typename T,
-          typename = std::enable_if_t<std::is_base_of_v<RefCount, T>>>
+          typename = std::enable_if_t<std::is_base_of_v<RefCounted, T>>>
 class WeakPtr
 {
 public:
@@ -182,23 +210,26 @@ public:
     {
     }
 
+    /// Construct from a raw pointer
+    WeakPtr(T* ptr_) :
+        ptr(nullptr),
+        refCount(nullptr)
+    {
+        *this = ptr_; // See operator= overload below.
+    }
+
+    /// Construct from another WeakPtr
     WeakPtr(const WeakPtr<T>& ptr_) :
         ptr(nullptr),
         refCount(nullptr)
     {
-        *this = ptr_;
+        *this = ptr_; // See operator= overload below.
     }
 
+    /// Construct from a SharedPtr
     WeakPtr(const SharedPtr<T>& ptr_) :
         ptr(nullptr),
-        refCount(nullptr)
-    {
-        *this = ptr_;
-    }
-
-    WeakPtr(T* ptr_) :
-        ptr(nullptr),
-        refCount(nullptr)
+        refCount(nullptr) // See operator= overload below.
     {
         *this = ptr_;
     }
@@ -211,42 +242,47 @@ public:
     /// Assigned from raw pointer
     WeakPtr<T>& operator = (T* rhs)
     {
-        if (Get() == rhs)
+        if (Get() == rhs) // Self check.
             return *this;
 
         Reset();
-        ptr = rhs;
-        refCount = ptr ? ptr->RefCountPtr() : nullptr;
+        ptr = rhs; // Just copy the object poiner.
+        refCount = ptr ? ptr->RefCountPtr() : nullptr; // copy RefCount if not nullptr
         if (refCount)
-            ++(refCount->weakRefs);
+            ++(refCount->weakRefs); // Add weak reference if not nullptr
         return *this;
     }
 
+    /// Assign from another WeakPtr.
     WeakPtr<T>& operator = (const WeakPtr<T>& rhs)
     {
         if (*this == rhs)
             return *this;
 
         Reset();
-        ptr = rhs.ptr;
+        ptr = rhs.ptr; // Just copy pointer as above
         refCount = rhs.refCount;
         if (refCount)
             ++(refCount->weakRefs);
         return *this;
     }
 
+    /// Assign from a SharedPtr.
     WeakPtr<T>& operator = (const SharedPtr<T>& rhs)
-    {   if (Get() == rhs.ptr)
+    {   if (*this == rhs)
             return *this;
 
         Reset();
-        ptr = rhs.Get();
+        ptr = rhs.Get(); // Just get and copy pointer as above.
         refCount = ptr ? ptr->RefCountPtr() : nullptr;
         if (refCount)
             ++(refCount->weakRefs);
         return *this;
     }
 
+    /// Release a weak reference and reset object poiner.
+    /// Notices that object may have been deconstructed.
+    /// Free the RefCount structure if this is the last weak reference.
     void Reset()
     {
         if (refCount)
@@ -258,17 +294,18 @@ public:
             refCount = nullptr;
         }
     }
-
+    
     template <typename U>
     void StaticCast(const WeakPtr<U>& rhs)
     {
-        *this = static_cast<T*>(rhs.ptr);
+        *this = static_cast<T*>(rhs.ptr); // operator= overload
     }
 
     template <typename U>
     void DynamicCast(const WeakPtr<U>& rhs)
     {
-        Reset();
+        Reset(); // dynamic_cast below may failed and return nullptr.
+        //  So reset first to preven previous object's memory leak.
         T* rhsObject = dynamic_cast<T*>(rhs);
         if (rhsObject)
             *this = rhsObject;
@@ -287,6 +324,7 @@ public:
     /// Return object pointer
     T* Get() const 
     {
+        // Pointed Object may have been deconstructed.
         if (refCount && !refCount->expired)
             return ptr;
         else
@@ -298,11 +336,30 @@ public:
     /// Return weak reference count
     unsigned WeakRefs() const { return ptr ? ptr->WeakRefs() : 0; }
     /// Return if it is a nullptr
-    bool IsNull() const { return ptr == nullptr; } 
+    bool IsNull() const { return ptr == nullptr; }
+    /// Return whether the pointed object has been destroyed.
     bool IsExpired() const { return refCount && refCount->expired; }
 private:
+    /// Object pointer.
     T* ptr;
+    /// Reference counting structure.
     RefCount* refCount;
 };
+
+template <typename T, typename U>
+WeakPtr<T> StaticCast(const WeakPtr<U>& rhs)
+{
+    WeakPtr<T> ret;
+    ret.StaticCast(rhs);
+    return ret;
+}
+
+template <typename T, typename U>
+WeakPtr<T> DynamicCast(const WeakPtr<U>& rhs)
+{
+    WeakPtr<T> ret;
+    ret.DynamicCast(rhs);
+    return ret;
+}
 
 } // namespace Pt
--- Phaten/main.cpp
+++ Phaten/main.cpp
@@ -3,7 +3,13 @@
 
 using namespace Pt;
 
+class Entity : public RefCounted {};
+
 int main()
 {
+    SharedPtr<Entity> entity = SharedPtr<Entity>(new Entity);
+
+    WeakPtr<Entity> weak = WeakPtr<Entity>(entity);
+
     return 0;
 }
